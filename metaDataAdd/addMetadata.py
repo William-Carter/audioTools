e4b5fd69ed638175c6c4b8f88236f8075e884528
William-Carter <alatreph@gmail.com>
2022-10-04 23:48:22 +1000

    Added extra functions for disc number and album artist

--- metaDataAdd/addMetadata.py
+++ metaDataAdd/addMetadata.py
@@ -23,3 +23,13 @@ def addArt(mp3Path, artPath):
     skeleton.addMetadata(
         mp3Path, artPath, "ffmpeg -i \"%s\" -i \"%s\" -map 0:0 -map 1:0 -c copy -id3v2_version 3 -metadata:s:v title=\"Album cover\" -metadata:s:v comment=\"Cover(front)\" \"%s\""
     )
+
+def addDiscNumber(mp3Path, cdNumber):
+    skeleton.addMetadata(
+        mp3Path, cdNumber, "ffmpeg -i \"%s\" -map 0 -c copy -id3v2_version 3 -write_id3v2 1 -metadata \"discNumber=%s\" \"%s\""
+    )
+
+def addAlbumArtist(mp3Path, artist):
+    skeleton.addMetadata(
+        mp3Path, artist, "ffmpeg -i \"%s\" -map 0 -c copy -id3v2_version 3 -write_id3v2 1 -metadata \"albumArtist=%s\" \"%s\""
+    )
